59f177186ac3fd9961170c50b25f2dfb80e9141f
mjuniper <mjuniper@gmail.com>
2014-09-29 13:52:13 -0600

    integrating

--- app/elements/esri_od-search-field/esri_od-search-field.js
+++ app/elements/esri_od-search-field/esri_od-search-field.js
@@ -6,10 +6,67 @@
     autofocus: true,
     placeholder: 'Search for Open Data',
     searchString: '',
+    searchEl: 'esri_od-search',
+
+    domReady: function () {
+      var url = document.querySelector(this.searchEl).url;
+      //this.initTypeahead(url);
+    },
 
     onSubmit: function (e) {
       e.preventDefault();
+      this.typeahead.typeahead('close');
       this.fire('esri_od:search:before-start', this.searchString);
+    },
+
+    getTypeaheadUrl: function (url, query) {
+      url = url.replace('%QUERY', query);
+      return url;
+    },
+
+    initBloodhound: function (url) {
+      var bloodhound = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+          url: url + 'autocomplete?query=%QUERY',
+          rateLimitWait: 150,
+          replace: self.getTypeaheadUrl,
+          filter: function(response) {
+            return response.data;
+          }
+        }
+      });
+
+      bloodhound.initialize();
+
+      return bloodhound;
+    },
+
+    initTypeahead: function (url) {
+
+      var self = this;
+
+      var opts = {
+        highlight: true,
+        minLength: 2,
+        hint: false
+      };
+
+      var bloodhound = this.initBloodhound(url);
+
+      var datasets = {
+        name: 'datasets',
+        displayKey: function(item) {
+          return item;
+        },
+        templates: {
+          empty: '<div class="empty-message">No datasets found.</div>'
+        },
+        source: bloodhound.ttAdapter()
+      };
+
+      this.typeahead = $(this.$.search).typeahead(opts, datasets).on('typeahead:selected', self.onSubmit.bind(this));
     }
 
   });
